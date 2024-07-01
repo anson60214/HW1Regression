9a4d93a25e4deea902ee54804441078780f1b62d
anson60214 <anson60214@gmail.com>
2024-06-30 23:58:06 -0700

    1

--- Pytorch/Fundamental/variables/variables.py
+++ Pytorch/Fundamental/variables/variables.py
@@ -4,4 +4,20 @@ from torch.autograd import Variable
 tensor = torch.FloatTensor([[1, 2], [3, 4]])
 variable = Variable(tensor, requires_grad= True)
 
-print(variable)
+print(f'tensor: {tensor}')
+print(f'variable: {variable}')
+
+t_out = torch.mean(tensor*tensor) # x^2
+v_out = torch.mean(variable*variable) 
+
+print(f't_out: {t_out}')
+print(f'v_out: {v_out}')
+
+v_out.backward()
+# v_out = 1/4* sum(var*var)
+# d(v_out)/ d(var) = 1/4* 2* var = 1/2 * var
+print(f'variable.grad: {variable.grad}')
+
+print(f'To tensor: {variable.data}')
+print(f'To numpy: {variable.data.numpy()}')
+
